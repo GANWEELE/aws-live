19a51af6caea6bf0538145a0061ad80fca22c261
GANWEELE <ganwl-wm23@student.tarc.edu.my>
2025-01-02 00:34:49 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.ciyjtgorcdaf.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "weele-employee"
+customregion = "us-east-1"
+
